13746ab58e633504bfd40aec2300b683c082aa9e
Javis Sullivan <javissullivan@gmail.com>
2015-12-21 16:08:37 -0500

    Added some structure

---
+++ app/routes.js
@@ -0,0 +1,47 @@
+// app/routes.js
+
+module.exports = function(app, passport) {
+  // homepage
+  app.get('/', function(req, res) {
+    res.render('index.html');
+  });
+
+  // login
+  app.get('/login', function(req, res) {
+    res.render('login.html');
+  });
+
+  // process login
+  // app.post('/login', blah blah passport);
+
+  // signup
+  app.get('/signup', function(req, res) {
+    res.render('signup.html', {message: req.flash('signupMessage')});
+  });
+
+  // process signup
+  // app.post('/signup', passport );
+
+  //profile section
+  app.get('/profile', isLoggedIn, function(req, res) {
+    res.render('profile', {
+      user: req.user // get the user out of session and pass template
+    });
+  });
+
+  //logout
+  app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+};
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+
+  // if they aren't redirect them to home page
+  res.redirect('/');
+}
---
+++ server.js
@@ -0,0 +1,43 @@
+// server.js
+
+// The Setup
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 8080,
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    flash = require('connect-flash'),
+    morgan = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    session = require('express-session'),
+    configDB = require('./config/database.js'),
+    nunjucks = require('nunjucks');
+
+// Configuration
+
+mongoose.connect(configDB.url); // connect to database
+
+// require('./configure/passport')(passport); // pass passport for configuration
+//setup express
+app.use(morgan('dev')); // log every request to console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); //get info from html forms
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+});
+
+// required for passport
+app.use(session({secret: 'notsosecretafterall'})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login session
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes
+require('./app/routes.js')(app, passport); // load our routes and pass in app and configured passport
+
+// launch
+app.listen(port);
+console.log('Party on port %s', port);
